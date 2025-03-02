c167392415bc8db5dceafd90f287342e5d770246
Igor Neres <igoritalo9698@gmail.com>
2025-03-02 19:12:52 -0300

    Mensagem do commit

---
+++ login.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="node_modules\bootstrap\comp\bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+<body style="overflow: hidden;">
+    <div class="container-fluid">
+        <div class="row min-vh-100 d-flex align-items-stretch ">
+            <div class="col-5 bg-success">
+                <div class="row">
+                    <img src="img/manoelmanologowhite.png" alt="" class="img-fluid w-100" style="margin-top: 25%; margin-left: 2%;">
+                </div>
+                <div class="row">
+                    <img src="img/cearalogowhite.png" alt="" class="img-fluid w-50" style="margin-top: 25%; margin-left: 30%;">
+                </div>
+            </div>
+            <div class="col text-center" style="margin-left: -22px; margin-top: -5px;">
+                <img src="img/onda1.png" class="img-fluid h-100">
+            </div>
+            <div class="col-5">
+                <div class="row">
+                    <img src="img/titulo.png" alt="" class="img-fluid w-50 rounded mx-auto d-block" style="margin-top: 10%;">
+                </div>
+                <div class="row">
+                    <img src="img/login.png" alt="" class="img-fluid w-25 rounded mx-auto d-block" style="margin-top: 15%;">
+                </div>
+                <form>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Usuário:</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Senha:</label>
+                    <input type="password" class="form-control">
+                </div>
+                <button type="submit" class="btn btn-outline-dark"><i class="bi bi-box-arrow-in-right"></i> Entrar</button>
+                </form>
+            </div>
+            <div class="col text-center" style="margin-right: -45px; margin-top: -5px;">
+                <img src="img/onda2.png" class="img-fluid h-100">
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
